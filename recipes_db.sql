865c75362e12ddd23c2cf270f73b10a655630cb9
gconner-code <gconner@trinity.edu>
2026-05-01 14:30:47 -0500

    Update recipes_db.sql

--- recipes_db.sql
+++ recipes_db.sql
@@ -20,6 +20,30 @@ CREATE TABLE recipe_ingredients (
 );
 
 INSERT INTO ingredients (name, info) VALUES
-('Salt', 'Essential seasoning'),
-('Chicken', 'Cook thoroughly'),
-('Rice', 'Staple grain');
+('Salt', 'Essential 1'),
+('Pepper', 'Essential 2'),
+('Chicken', 'DO NOT UNDERCOOK'),
+('Rice', 'Generic carb'),
+('Grlic', 'Vampire repellent'),
+('Oil', 'Use any kind you want'),
+('Beef', 'The best'),
+('Tofu', 'Plant?');
+
+INSERT INTO recipes (name, protein, intructions) VALUES
+('Chicken & rice bowl', 'Chicken', 'Seacon the chicken with salt and pepper. Fry the rice in the oil of your choice and add garlic for flavor.'),
+('Just beef', 'Beef', 'Just cook ground beef with salt and pepper.'),
+('Tofu stir fry', 'Tofu', 'fry the tofu and some rice in oil.');
+
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id) VALUES
+(1,3),
+(1,1),
+(1,2),
+(1,4),
+(1,6),
+(1,5),
+(2,7),
+(2,1),
+(2,2),
+(3,8),
+(3,4),
+(3,6);
